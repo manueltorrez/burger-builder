7789491630b8c2be29f11742533685c26b9e13af
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-01-06 12:10:18 -0600

    Creating a Layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Awita de uwu</h1>
+        <Layout>
+          <p>This is working, indeed.</p>
+        </Layout>
       </div>
     );
   }
