e0fbd0b56238d8869886565ce8c8cc0b017cb333
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-01-13 09:21:37 -0600

    Sending a POST request to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -46,7 +47,29 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue... It is a long trip to home.");
+    //alert("You continue... It is a long trip to home.");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "John Wick",
+        address: {
+          street: "Test street 1",
+          zipCode: "8920",
+          country: "USA"
+        },
+        email: "john@wick.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   addIngredientHandler = type => {
