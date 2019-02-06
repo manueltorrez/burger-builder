dc24185cfb591a7c65bab4ae2fb1e3716a1bd64d
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-02-06 12:07:15 -0600

    Sa

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -6,7 +6,26 @@ const initalState = {
 }
 
 const reducer = (state = initalState, action) => {
+    switch(action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            }
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            }
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {
+                ...state,
+                loading: false
 
+            }
+        default:
+            return state;
+    }
 }
 
 export default reducer;
