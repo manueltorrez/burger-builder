85e678435974a805717f7f679b2f47a3b8d832fc
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-02-06 12:05:49 -0600

    The order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,12 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initalState = {
+    orders: [],
+    loading: false
+}
+
+const reducer = (state = initalState, action) => {
+
+}
+
+export default reducer;
