04b216ad5fb2ea3e3ab9542183aef57058b722f7
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2019-02-11 12:45:35 -0600

    Refactoring reducers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,7 +8,6 @@ import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import axios from "../../axios-orders";
-import * as actionTypes from "../../store/actions/actionTypes";
 import * as actions from "../../store/actions/index";
 
 class BurgerBuilder extends Component {
